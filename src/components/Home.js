389279d86a210278a5ba4ef006e05f1bfb6e17fd
YutakaHub <92905563+YutakaHub@users.noreply.github.com>
2023-10-04 20:14:19 +0900

    記事投稿機能実装

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+import './Home.css'
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
-function Home() {
-  return <div>Home</div>;
-  };
+const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPost = async () =>{
+      const data = await getDocs(collection(db,"posts"));
+      //console.log(data);
+      //console.log(data.docs.map((doc) => ({doc})));
+      //console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+    getPost()
+  },[]);
+
+
+  return (
+  <div className='homePage'>
+    {postList.map((post) => {
+      return (
+        <div className='postContents' key={post.id}>
+          <div className="postHeader">
+            <h1>{post.title}</h1>
+          </div>
+          <div className='postTextContainer'>
+            {post.postsText}
+          </div>
+          <div className='nameAndDeleteButton'>
+            <h3>{post.author.username}</h3>
+            <button>削除</button>
+          </div>
+        </div>
+      );
+    })}
+  </div>
+  )};
 
 export default Home;
